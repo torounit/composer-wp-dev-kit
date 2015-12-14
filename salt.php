92aef3965e0e6040a05f8ef667f0643db6891a79
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2015-12-15 02:29:43 +0900

    remove salt

--- salt.php
+++
@@ -1,9 +0,0 @@
-<?php 
-define('AUTH_KEY',         'Q*5}e0Qe]E.Zbo!QtxmYRoF])-:#+h/tp!+}}jNG5Tqc/4i8BWH+IAn7Hi$:D-o!');
-define('SECURE_AUTH_KEY',  'W$DBKJ@$xx*j7b&z{;=t$XMSxY7ElRa#L$FZ7vaTewmtsq8p{OsOkK?G$JG.K c3');
-define('LOGGED_IN_KEY',    'f1 ?8]{:houb3_yo)(Or+O>R[{HCKX@X$,zE>uv]%uv,^!5JV XiNI^ou<wdUYpN');
-define('NONCE_KEY',        '!k!*)^/&hiSDl&Sv%z2$3k|o@~s8-a|)kZZ1/U-FA^S_i-<JP#/!},y<9f6L*WCf');
-define('AUTH_SALT',        'lcAY0:6nE9L1lJQupe]zVNr4wRei1H[o`NAvGSUWAy%O*!6oo;0$,|$p:3P}q++=');
-define('SECURE_AUTH_SALT', 'UjIba.+o(CLlJb$?FpDm3stwiSy>^w.jRO d#yb|YRt-q%Kcikg2+!hJC:UFl,@0');
-define('LOGGED_IN_SALT',   '>o@642Q|RpfF9wR2Eg+wEYrC|D1%@q9+=XE-1t)brugdef;R/#iIEKY]SgSSPD9U');
-define('NONCE_SALT',       'l~`uc; 11r:I`jOI=p]u2J|wR=VRh4!IY+`gePa1Q=zjfpDp?q>%|j>uhz4?8S$>');
