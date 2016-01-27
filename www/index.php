71290f18cedccb32bf3c682a6fb5eae124b780f9
Hiroshi Urabe <mail@torounit.com>
2016-01-28 04:48:13 +0900

    fix bug

--- www/index.php
+++ www/index.php
@@ -12,5 +12,6 @@
  */
 define('WP_USE_THEMES', true);
 
+
 /** Loads the WordPress Environment and Template */
-//require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
