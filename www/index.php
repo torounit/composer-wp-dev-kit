23e3382855b5075dd7fd9549463a9c237d9dfcd6
Hiroshi Urabe <mail@torounit.com>
2016-01-28 01:04:08 +0900

    fix wp cli bug

--- www/index.php
+++ www/index.php
@@ -5,7 +5,6 @@
  *
  * @package WordPress
  */
-
 /**
  * Tells WordPress to load the WordPress theme and output it.
  *
@@ -14,4 +13,4 @@
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+//require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
